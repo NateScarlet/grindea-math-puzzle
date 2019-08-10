6658c5b03424c6e923ba373ae039142b4771889f
NateScarlet <NateScarlet@Gmail.com>
2019-08-11 01:46:33 +0800

    chore: deploy
    [skip ci]

---
+++ precache-manifest.0ef1f225adb97ea0e5b2211472e23c6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "cbac49f0adba0acd2c64",
+    "url": "js/chunk-vendors.665b82dc.js"
+  },
+  {
+    "revision": "5c357918f2bf012e6fb3",
+    "url": "js/app.ac837d38.js"
+  },
+  {
+    "revision": "ed6ec55fa1cf1eaef2bb8b06024685f8",
+    "url": "index.html"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "cbac49f0adba0acd2c64",
+    "url": "css/chunk-vendors.83f7ecef.css"
+  },
+  {
+    "revision": "5c357918f2bf012e6fb3",
+    "url": "css/app.8fdda62f.css"
+  }
+];
