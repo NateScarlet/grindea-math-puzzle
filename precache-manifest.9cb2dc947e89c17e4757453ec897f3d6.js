3632ed95639ebaf0c78e852c20c2575f41c8426c
CI User <>
2021-05-28 09:52:40 +0000

    chore: deploy
    [skip ci]

--- precache-manifest.9cb2dc947e89c17e4757453ec897f3d6.js
+++ precache-manifest.9cb2dc947e89c17e4757453ec897f3d6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "744a619cf90974e86cf9",
+    "revision": "33b92059a7f4e4afb15c",
     "url": "css/app.ec0afb7e.css"
   },
   {
-    "revision": "931a5abe475e1221243c",
-    "url": "css/chunk-vendors.3be75749.css"
+    "revision": "680602ba47b320fd06f4",
+    "url": "css/chunk-vendors.a9c53725.css"
   },
   {
     "revision": "535877f50039c0cb49a6196a5b7517cd",
@@ -16,16 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "fonts/element-icons.732389de.ttf"
   },
   {
-    "revision": "c1da8371fb633dae2a44ab0ce5cfb59b",
+    "revision": "9e10b7d8d93e8a773db32650b61313c7",
     "url": "index.html"
   },
   {
-    "revision": "744a619cf90974e86cf9",
-    "url": "js/app.cc807e85.js"
+    "revision": "33b92059a7f4e4afb15c",
+    "url": "js/app.e5281290.js"
   },
   {
-    "revision": "931a5abe475e1221243c",
-    "url": "js/chunk-vendors.29f579f9.js"
+    "revision": "680602ba47b320fd06f4",
+    "url": "js/chunk-vendors.e1317608.js"
   },
   {
     "revision": "bbfbbc6086d5f94e9408eef2ffcc99f9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8841354fe59d22def662f074aab51859.js"
+  "precache-manifest.9cb2dc947e89c17e4757453ec897f3d6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "grindea-math-puzzle"});
